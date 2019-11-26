0429fe266f5dd3c2c33a5edbcaae2b3967674f14
robyafrizal <robyafrizal86@gmail.com>
2019-11-26 10:05:24 +0700

    React Redux Kelompok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import {
+  Button,
+  Container,
+  Header,
+  List,
+  Menu,
+  Segment
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Segment inverted vertical textAlign="center">
+        <BrowserRouter>
+          <Container as="nav">
+            <Menu fixed="top" borderless compact inverted>
+              <Menu.Item active>
+                <Link to="/">Home</Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link to="/todo">To Do</Link>
+              </Menu.Item>
+            </Menu>
+          </Container>
+
+          <Switch>
+            <Route exact path="/">
+              <Container className="content">
+                <Header inverted as="h1">
+                  Project Redux Kelompok
+                </Header>
+                <p>About To Do wtih axios and react-redux</p>
+                <Button size="huge">Learn more</Button>
+              </Container>
+            </Route>
+            <Route path="/todo">
+              <Container text style={{ marginTop: "7em" }}>
+                <Todo />
+              </Container>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+
+        <Segment inverted vertical as="footer">
+          <Container textAlign="center">
+            <List horizontal divide link size="small">
+              <p>Project Kelompok Redux @ 2019</p>
+            </List>
+          </Container>
+        </Segment>
+      </Segment>
     </div>
   );
 }
